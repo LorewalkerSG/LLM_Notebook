866ec22602c68d970e8739109d16a384ceff4d56
lorewalker <m757474355@gmail.com>
2026-05-07 11:09:02 +0800

    a

--- agent/langchain-01.py
+++ agent/langchain-01.py
@@ -11,8 +11,8 @@ os.environ["https_proxy"] = proxy_url
 os.environ["HTTP_PROXY"] = proxy_url
 os.environ["HTTPS_PROXY"] = proxy_url
 
-# 2. 🚨 必须填入你的真实 API KEY
-HF_TOKEN = ""
+
+
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = HF_TOKEN
 
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
